c617270f2dd591ac5ceeafd67f512d725da3bcd5
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-24 18:56:40 +0300

    💚 Implement OpenZeppelin ERC20

---
+++ contracts/OurToken.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    //initial suppy is 50 -> 50 WEI
+    //initial supply 50e18
+    //50 * 10**18
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply)
+    }
+}
